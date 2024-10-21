7a2172b7f12ae6fde55a50034849c435cb5aaa23
Dimitrios <dimitrios.gkiokmema@mail.utoronto.ca>
2024-10-21 14:00:37 -0400

    Logout TODOs

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,9 +4,14 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
+    private final String name;
 
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        // Done TODO: save the current username in an instance variable and add a getter.
+        this.name = username;
     }
 
+    public String getName() {
+        return name;
+    }
 }
